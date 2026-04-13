555df1e0701747ab932ce6f908b0fa57b3d049c3
絵夢羽さ沂 <mausu2526@gmail.com>
2026-04-13 13:11:23 +0800

    Feat: Add 35 New Recommended Notes for Lv.50-90 Sets

--- src/data/recommended/index.ts
+++ src/data/recommended/index.ts
@@ -5,14 +5,19 @@ import lv100_ilv710 from './lv100_ilv710.json';
 import lv100_ilv720 from './lv100_ilv720.json';
 import lv100_ilv720_690 from './lv100_ilv720_690.json';
 import lv50_ilv110 from './lv50_ilv110.json';
+import lv50_ilv115 from './lv50_ilv115.json';
 import lv60_ilv190 from './lv60_ilv190.json';
+import lv60_ilv255 from './lv60_ilv255.json';
 import lv60_ilv255_250 from './lv60_ilv255_250.json';
 import lv70_ilv340 from './lv70_ilv340.json';
 import lv70_ilv380 from './lv70_ilv380.json';
+import lv70_ilv385 from './lv70_ilv385.json';
 import lv80_ilv490 from './lv80_ilv490.json';
 import lv80_ilv510 from './lv80_ilv510.json';
+import lv80_ilv515 from './lv80_ilv515.json';
 import lv90_ilv620 from './lv90_ilv620.json';
 import lv90_ilv640 from './lv90_ilv640.json';
+import lv90_ilv645 from './lv90_ilv645.json';
 
 const allRecommendedNotes: Note[] = [
   ...(lv100_ilv690 as Note[]),
@@ -20,14 +25,19 @@ const allRecommendedNotes: Note[] = [
   ...(lv100_ilv720 as Note[]),
   ...(lv100_ilv720_690 as Note[]),
   ...(lv50_ilv110 as Note[]),
+  ...(lv50_ilv115 as Note[]),
   ...(lv60_ilv190 as Note[]),
+  ...(lv60_ilv255 as Note[]),
   ...(lv60_ilv255_250 as Note[]),
   ...(lv70_ilv340 as Note[]),
   ...(lv70_ilv380 as Note[]),
+  ...(lv70_ilv385 as Note[]),
   ...(lv80_ilv490 as Note[]),
   ...(lv80_ilv510 as Note[]),
+  ...(lv80_ilv515 as Note[]),
   ...(lv90_ilv620 as Note[]),
   ...(lv90_ilv640 as Note[]),
+  ...(lv90_ilv645 as Note[]),
 ];
 
 export default allRecommendedNotes;
