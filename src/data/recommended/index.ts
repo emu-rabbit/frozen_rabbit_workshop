9d8a5d2c1cc3869482ef03016a047b6e0e8de409
絵夢羽さ沂 <mausu2526@gmail.com>
2026-04-13 13:05:19 +0800

    Refactor: Rename Recommended Data Files to ilv for Consistency

--- src/data/recommended/index.ts
+++ src/data/recommended/index.ts
@@ -1,35 +1,33 @@
 import type { Note } from '../../types/note';
 
-import lv50_il110 from './lv50_il110.json';
-import lv60_il190 from './lv60_il190.json';
-import lv60_il255_250 from './lv60_il255_250.json';
-import lv70_il340 from './lv70_il340.json';
-import lv70_il380 from './lv70_il380.json';
-import lv80_il490 from './lv80_il490.json';
-import lv80_il510 from './lv80_il510.json';
-import lv90_il620 from './lv90_il620.json';
-import lv90_il640 from './lv90_il640.json';
-import lv100_il690 from './lv100_il690.json';
-import lv100_il710 from './lv100_il710.json';
-import lv100_il720_690 from './lv100_il720_690.json';
-import lv100_il720 from './lv100_il720.json';
+import lv100_ilv690 from './lv100_ilv690.json';
+import lv100_ilv710 from './lv100_ilv710.json';
+import lv100_ilv720 from './lv100_ilv720.json';
+import lv100_ilv720_690 from './lv100_ilv720_690.json';
+import lv50_ilv110 from './lv50_ilv110.json';
+import lv60_ilv190 from './lv60_ilv190.json';
+import lv60_ilv255_250 from './lv60_ilv255_250.json';
+import lv70_ilv340 from './lv70_ilv340.json';
+import lv70_ilv380 from './lv70_ilv380.json';
+import lv80_ilv490 from './lv80_ilv490.json';
+import lv80_ilv510 from './lv80_ilv510.json';
+import lv90_ilv620 from './lv90_ilv620.json';
+import lv90_ilv640 from './lv90_ilv640.json';
 
 const allRecommendedNotes: Note[] = [
-  ...(lv50_il110 as Note[]),
-  ...(lv60_il190 as Note[]),
-  ...(lv60_il255_250 as Note[]),
-  ...(lv70_il340 as Note[]),
-  ...(lv70_il380 as Note[]),
-  ...(lv80_il490 as Note[]),
-  ...(lv80_il510 as Note[]),
-  ...(lv90_il620 as Note[]),
-  ...(lv90_il640 as Note[]),
-  ...(lv100_il690 as Note[]),
-  ...(lv100_il710 as Note[]),
-  ...(lv100_il720_690 as Note[]),
-  ...(lv100_il720 as Note[]),
+  ...(lv100_ilv690 as Note[]),
+  ...(lv100_ilv710 as Note[]),
+  ...(lv100_ilv720 as Note[]),
+  ...(lv100_ilv720_690 as Note[]),
+  ...(lv50_ilv110 as Note[]),
+  ...(lv60_ilv190 as Note[]),
+  ...(lv60_ilv255_250 as Note[]),
+  ...(lv70_ilv340 as Note[]),
+  ...(lv70_ilv380 as Note[]),
+  ...(lv80_ilv490 as Note[]),
+  ...(lv80_ilv510 as Note[]),
+  ...(lv90_ilv620 as Note[]),
+  ...(lv90_ilv640 as Note[]),
 ];
 
-// Sort by date or other criteria if needed, 
-// here we keep it simple as it's aggregated by version.
 export default allRecommendedNotes;
